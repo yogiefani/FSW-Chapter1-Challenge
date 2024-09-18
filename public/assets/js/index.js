11fd62d1591c23436e633ddb75837f686ff2c5e4
yogiefani <yogiefaniyc@gmail.com>
2024-09-18 23:02:29 +0700

    fix: add all section

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,12 @@
+// sidebar
+document.getElementById('openSidebar').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.add('show');
+});
+document.getElementById('closeSidebar').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.remove('show');
+});
+
+// swipper
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -15,11 +24,15 @@ var swiper = new Swiper('.swiper-container', {
             spaceBetween: 20,
         },
         768: {
-            slidesPerView: 2,
-            spaceBetween: 40,
+            slidesPerView: 1, /* pastikan satu slide untuk mobile */
+            spaceBetween: 20,
         },
         1024: {
-            slidesPerView: 3,
+            slidesPerView: 2, /* sesuaikan untuk desktop */
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 3, /* sesuaikan untuk desktop lebar */
             spaceBetween: 50,
         },
     }
